fb6c434d104c0ac0b6ac783f6feb56cdf6b9d572
Kerry Ritter <ritter@kerryritter.com>
2020-11-27 20:40:07 -0600

    bugfix(transformer-remove): fixing the transformer when calling remove

--- src/lib/class-transformer.middleware.ts
+++ src/lib/class-transformer.middleware.ts
@@ -1,11 +1,20 @@
+import { Logger } from '@nestjs/common';
 import { plainToClass } from 'class-transformer';
 import { ModelType } from './types';
+
+const logger = new Logger('classTransformerMiddlewareFactory');
 
 export function classTransformerMiddlewareFactory<T>(type: ModelType<T>) {
   return context => next => (args, method) => {
     return next(args, method).then(res => {
-      if (res) {
-        res = plainToClass(type, res);
+      if (res && method !== 'remove') {
+        try {
+          res = plainToClass(type, res);
+        } catch (ex) {
+          res = null;
+          const error = ex as Error;
+          logger.error(ex.message, ex.stack);
+        }
       }
       return res;
     });
